3d7776ed270ca41992a89f6d433d88b5c002eef6
younghwan12 <dowk3432@gmail.com>
2023-03-24 12:51:48 +0900

    데이터 불러와지는지 확인중

--- src/features/main/component/MainSearch/index.tsx
+++ src/features/main/component/MainSearch/index.tsx
@@ -1,26 +1,20 @@
+import ISearchResList from "@/mocks/camp/campMock";
 import { useState, useEffect } from "react";
 import {
     useGetCampListQuery,
     useLazyGetCampListQuery,
 } from "../../redux/mainApi";
 
-interface Product {
-    id: string;
-    name: string;
-    quantity: number;
-}
-
-interface Review {
-    id: string;
-    author: string;
-    content: string;
-}
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 const MainSearch = () => {
-    const [getCamp, { data: CampList }] = useLazyGetCampListQuery();
+    const [getCamp, { data: CampList, isFetching }] = useLazyGetCampListQuery();
+    const [data, setData] = useState<ISearchResList[]>();
+    const [selectedDatas, setSelectedDatas] = useState([]);
 
     useEffect(() => {
-        console.log(CampList);
+        setData(CampList);
     }, [CampList]);
 
     const handleGetReview = () => {
@@ -30,6 +24,19 @@ const MainSearch = () => {
     return (
         <>
             <button onClick={handleGetReview}>msw 확인용</button>
+            <DataTable
+                value={data}
+                loading={isFetching}
+                selectionMode="checkbox"
+                selection={selectedDatas}
+                onSelectionChange={(e) => setSelectedDatas(e.value)}
+                paginator
+                rows={5}
+            >
+                <Column field="사진" header="사진" />
+                <Column field="금액" header="금액" />
+                <Column field="별점" header="별점" />
+            </DataTable>
         </>
     );
 };
